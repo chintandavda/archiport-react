3dcc63a3daca0e1636e6529811b3ed58d7b5230d
chintandavda <davdachintan97@gmail.com>
2024-08-07 16:01:01 +0530

    added links in env file

--- src/services/DesignPostAPI.js
+++ src/services/DesignPostAPI.js
@@ -1,7 +1,10 @@
 // src/services/DesignPostAPI.jsx
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:3001";
+const API_BASE_URL = process.env.NODE_DESIGN_SERVICE;
+
+// NODE_DESIGN_SERVICE = http://127.0.0.1:8000/api/
+
 
 const api = axios.create({
     baseURL: API_BASE_URL,
